0ef87644ebf8735ba48093721a076cb32118c506
enriball@ucm.es <enriball@ucm.es@075e9cc7-18cf-fbc0-bdc4-be8700552dcb>
2013-11-08 13:25:08 +0000

    hola iker del futuro

---
+++ trunk/Practica4/PRACTICA4_BBDD.sql
@@ -0,0 +1,83 @@
+
+CREATE TABLESPACE EMPRESA_BDD_15 DATAFILE 'D:\oracle\EMPRESA_BDD_15' SIZE 5M AUTOEXTEND OFF; 
+
+CREATE USER BDD15 IDENTIFIED BY BDD151314 DEFAULT TABLESPACE EMPRESA_BDD_15 TEMPORARY TABLESPACE 
+TEMP QUOTA UNLIMITED ON EMPRESA_BDD_15;
+
+GRANT CREATE SESSION, CREATE TABLE, DELETE ANY TABLE, SELECT ANY 
+DICTIONARY, CREATE ANY SEQUENCE TO BDD15;
+
+CREATE TABLE Restaurantes(
+  codigo Number(8) PRIMARY KEY,
+  nombre Char(20) NOT NULL,
+  calle Char(30) NOT NULL,
+  "codigo postal" Char(5) NOT NULL,
+  comision Number(8, 2)
+  ); 
+
+CREATE TABLE "Areas Cobertura" (
+  restaurante Number(8) REFERENCES Restaurantes(codigo),
+  "codigo postal" Char(5),
+  PRIMARY KEY(restaurante, "codigo postal")
+  );
+
+CREATE TABLE Horarios (
+  restaurante Number(8) REFERENCES Restaurantes(codigo),
+  "dia semana" Char(1) NOT NULL,
+  hora_apertura Date NOT NULL,
+  hora_cierre Date NOT NULL,
+  PRIMARY KEY(restaurante, "dia semana")
+);
+
+CREATE TABLE Platos (
+  restaurante Number(8),
+  nombre Char(20),
+  precio Number(8,2),
+  descripcion Char(30),
+  categoria Char(10),
+  PRIMARY KEY(restaurante, nombre)
+);
+
+CREATE TABLE Clientes (
+  DNI Char(9) PRIMARY KEY,
+  nombre Char(32) NOT NULL,
+  apellido Char(32) NOT NULL,
+  calle Char(20) NOT NULL,
+  numero Number(4) NOT NULL,
+  piso Char(5),
+  localidad Char(15),
+  "codigo postal" Char(5) NOT NULL,
+  telefono Char(9),
+  usuario Char(8) NOT NULL,
+  contraseña Char(8) NOT NULL
+);
+
+CREATE TABLE Pedidos (
+  codigo Number(8) PRIMARY KEY,
+  estado Char(9) NOT NULL,
+  fecha_hora_pedido Date NOT NULL,
+  fecha_hora_entrega Date,
+  "importe total" Number(8,2),
+  cliente Char(9) NOT NULL REFERENCES Clientes(DNI),
+  codigoDescuento Number(8) REFERENCES Descuentos(codigo) ON DELETE SET NULL
+);
+
+CREATE TABLE Contiene (
+  restaurante Number(8),
+  plato Char(20),
+  pedido Number(8) REFERENCES Pedidos(codigo) ON DELETE CASCADE,
+  "precio con comision" Number(8,2),
+  unidades Number(4) NOT NULL,
+  PRIMARY KEY (restaurante, plato, pedido),
+  FOREIGN KEY (restaurante, plato) REFERENCES Platos(restaurante, nombre)
+);
+
+CREATE TABLE Descuentos (
+  codigo Number(8) PRIMARY KEY,
+  fecha_caducidad Date NOT NULL,
+  "porcentaje descuento" Number(3) NOT NULL CHECK ("porcentaje descuento" >0 AND "porcentaje descuento"<=100)
+);
+
+CREATE INDEX I_CatPlatos ON Platos(categoria);
+CREATE SEQUENCE Seq_CodPedidos INCREMENT BY 1 START WITH 1
+NOMAXVALUE;
